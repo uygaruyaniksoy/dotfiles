08d04a448d725077040a3b590bf128cf1d8be70d
Uygar Uyanıksoy <uygaruyaniksoy@gmail.com>
2018-10-13 22:27:03 +0300

    rm chromium config files

--- .config/chromium/Default/Extensions/nonjdcjchghhkdoolnlbekcfllmednbl/6.0.2_0/js/viewTab.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.sendMessage({action:'getOptions'}, function (options) {
-    window.addEventListener('keydown', function (event) {
-        if (event.which == options.openImageInTabKey) {
-            window.close();
-        }
-    });
-});
--- .config/chromium/Default/Extensions/nonjdcjchghhkdoolnlbekcfllmednbl/6.0.2_0/plugins/live.js
+++
@@ -1,9 +0,0 @@
-﻿var hoverZoomPlugins = hoverZoomPlugins || [];
-hoverZoomPlugins.push({
-    name:'Live.com',
-    prepareImgLinks:function (callback) {
-        var res = [];
-        hoverZoom.urlReplace(res, 'img[src*="maxwidth="]', /&max(width|height)=\d+/g, '');
-        callback($(res));
-    }
-});
