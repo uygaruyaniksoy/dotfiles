b029fb32eacaa37504d91d8b2fda7345c7d81242
Uygar Uyanıksoy <uygaruyaniksoy@gmail.com>
2018-10-22 00:59:30 +0300

    floating window for gcaladd

--- .scripts/gcaladd.sh
+++ .scripts/gcaladd.sh
@@ -1,2 +1,7 @@
-echo "title: " && read -r input && echo "when: " && read -r inputt && gcalcli --calendar uygaruyaniksoy@gmail.com add --duration 60 --title "$(echo $input | sed -r 's/(.*)/\1/')" --when "$(echo $inputt | sed -r  's/(.*)/\1/')"
+echo "*** Add a new calendar item ***"
+echo "title: "
+read -r input
+echo "when: "
+read -r inputt
+gcalcli --calendar uygaruyaniksoy@gmail.com add --duration 60 --title "$(echo $input | sed -r 's/(.*)/\1/')" --when "$(echo $inputt | sed -r  's/(.*)/\1/')"
 exit
