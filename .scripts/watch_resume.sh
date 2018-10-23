c38444b0cbc8042629bf7774ec014d52cf9726aa
Uygar Uyanıksoy <uygaruyaniksoy@gmail.com>
2018-10-23 12:11:57 +0300

    resume works with dmenu

--- .scripts/watch_resume.sh
+++ .scripts/watch_resume.sh
@@ -1,8 +1,10 @@
 cd ~/Documents/resume
-while inotifywait --exclude ".*(log|pdf)$" -e modify ~/Documents/resume/; do
-    trash ~/Documents/resume/resume.pdf
+while inotifywait --exclude ".*(log|pdf|aux|out)$" -e modify ~/Documents/resume/; do
     killall zathura
+    trash ~/Documents/resume/resume.pdf
     pdflatex ~/Documents/resume/resume.tex > /dev/null
     zathura ~/Documents/resume/resume.pdf &
 done &
 vi ~/Documents/resume/resume.tex
+killall inotifywait
+exit
