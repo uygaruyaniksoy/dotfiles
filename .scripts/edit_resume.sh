a0c13be46b5d9db94d6c3e39b3805481789c27ff
Uygar Uyanıksoy <uygaruyaniksoy@gmail.com>
2018-10-23 12:59:50 +0300

    gcaladd notifies when succesfull

--- .scripts/edit_resume.sh
+++ .scripts/edit_resume.sh
@@ -1,8 +1,8 @@
 cd ~/Documents/resume
 while inotifywait --exclude ".*(log|pdf|aux|out)$" -e modify ~/Documents/resume/; do
-    killall zathura
     trash ~/Documents/resume/resume.pdf
     pdflatex ~/Documents/resume/resume.tex > /dev/null
+    killall zathura
     zathura ~/Documents/resume/resume.pdf &
 done &
 vi ~/Documents/resume/resume.tex
--- .scripts/gcaladd.sh
+++ .scripts/gcaladd.sh
@@ -3,5 +3,5 @@ echo "title: "
 read -r input
 echo "when: "
 read -r inputt
-gcalcli --calendar uygaruyaniksoy@gmail.com add --duration 60 --title "$(echo $input | sed -r 's/(.*)/\1/')" --when "$(echo $inputt | sed -r  's/(.*)/\1/')"
+gcalcli --calendar uygaruyaniksoy@gmail.com add --duration 60 --title "$(echo $input | sed -r 's/(.*)/\1/')" --when "$(echo $inputt | sed -r  's/(.*)/\1/')" && notify-send --icon=gtk-info "$inputt" "$input"
 exit
