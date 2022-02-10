ef1df086392c531f9c4ed4b1cba06a3e3f2c7227
Uygar Uyaniksoy <uygaruyaniksoy@gmail.com>
2022-02-10 13:03:05 +0300

    remove old links in the linking script

--- link.sh
+++ link.sh
@@ -1,6 +1,18 @@
 ROOT="$(dirname "$(readlink -fm "$0")")"
 
-ln -sf $ROOT/i3 ~/.config/i3
-ln -sf $ROOT/sxhkd ~/.config/sxhkd
-ln -sf $ROOT/scripts ~/.scripts
-ln -sf $ROOT/polybar ~/.config/polybar
+# remove old links
+rm -rf 				\
+	~/.config/i3 		\
+	~/.config/sxhkd     	\
+	~/.config/polybar 	\
+	~/.config/kitty     	\
+	~/.scripts
+
+# config
+ln -s $ROOT/i3 ~/.config/i3
+ln -s $ROOT/sxhkd ~/.config/sxhkd
+ln -s $ROOT/polybar ~/.config/polybar
+ln -s $ROOT/kitty ~/.config/kitty
+
+# scripts
+ln -s $ROOT/scripts ~/.scripts
